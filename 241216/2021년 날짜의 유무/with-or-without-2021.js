5240d728b8597618590d1402035b1b750850e303
kim13175 <100892364+kim13175@users.noreply.github.com>
2024-12-16 18:51:24 +0900

    Title: 2021년 날짜의 유무, Time: 186ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241216/2021년 날짜의 유무/with-or-without-2021.js
@@ -0,0 +1,20 @@
+function checkValidDate(month, day) {
+    if (month === 2) {
+        if (day > 28) return "No";
+    }
+    if (month % 2 !== 0) {
+        if (day > 31) return "No"
+    }
+    if (month % 2 === 0) {
+        if (day > 30) return "No"
+    }
+    return "Yes"
+}
+
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(" ")
+
+const month = input[0];
+const day = input[1];
+
+console.log(checkValidDate(month, day));
