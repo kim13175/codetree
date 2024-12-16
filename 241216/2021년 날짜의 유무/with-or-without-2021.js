844a8c5cd212cf777252c041b1c015cc642b526a
kim13175 <100892364+kim13175@users.noreply.github.com>
2024-12-16 19:48:48 +0900

    Title: 2021년 날짜의 유무, Time: 172ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241216/2021년 날짜의 유무/with-or-without-2021.js
+++ 241216/2021년 날짜의 유무/with-or-without-2021.js
@@ -1,32 +1,31 @@
 function checkValidDate() {
-    if (checkMonthPerDate() && validMonth()) return "Yes"; 
+    if (leapYear() && checkMonthPerDate() && validMonth()) return "Yes" 
     return "No";
 }
 
+// 윤년
+function leapYear() {
+    if (day > 28) return false;
+    return true;
+}
+
 function checkMonthPerDate() {
-    if (leapYear()) return true;
     if (month >= 8) {
         if (month % 2 === 0) {
-            if (day > 31) return false;
+            if (day <= 31) return true;
         }
-        if (day > 30) return false;
+        if (day <= 30) return true;
     }
-    if (month < 8) {
+    if (month < 8 && month !== 2) {
         if (month % 2 === 0) {
-            if (day > 30) return false;
+            if (day <= 30) return true;
         }
-        if (day > 31) return false;
+        if (day <= 31) return true;
     }
-    return true;
+    return false;
 }
 
-function leapYear() {
-    if (month === 2) {
-        if (day > 28) return false
-    };
-    return true;
-}
-
+// 유효한 개월 수
 function validMonth() {
     if (month <= 12) return true;
     return false;
@@ -38,4 +37,4 @@ const input = fs.readFileSync(0).toString().trim().split(" ").map(Number)
 const month = input[0];
 const day = input[1];
 
-console.log(checkValidDate(month, day));
+console.log(checkValidDate());
