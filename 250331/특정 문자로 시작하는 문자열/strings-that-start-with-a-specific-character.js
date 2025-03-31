08517cadcbe59ff7382c6a5d55549c91b6e52f65
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-03-31 16:11:38 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 180ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250331/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.js
+++ 250331/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.js
@@ -3,6 +3,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const stringCount = input[0];
 let count = 0;
+let lengthCount = 0;
 
 let stringArr = new Array(Number(stringCount));
 stringArr = input.slice(1, input.length - 1);
@@ -12,22 +13,16 @@ const checkAlphabet = (str) => {
 
     if (str[0] === alphabet) {
         count += 1;
+        lengthCount += str.length;
     }
 }
 
-const checkAvgLen = (stringArr) => {
-    let lengthCount = 0;
-
-    for (let i = 0; i < stringArr.length; i++) {
-        lengthCount += stringArr[i].length;
-    }
-
-    const avg = Math.round((lengthCount / stringCount), 2);
-    return avg;
+const checkAvgLen = () => {
+    return Math.round((lengthCount / count), 2).toFixed(2);     
 }
 
 for (let i = 0; i < stringCount; i++) {
     checkAlphabet(stringArr[i]);
 }
 
-console.log(count, checkAvgLen(stringArr).toFixed(2));
+console.log(count, checkAvgLen());
