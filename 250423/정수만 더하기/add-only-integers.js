81f4c528eb216960e294d6821b1eccefc2ae5872
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-04-23 10:45:43 +0900

    Title: 정수만 더하기, Time: 177ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250423/정수만 더하기/add-only-integers.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+
+let totalSum = 0;
+for (let i = 0; i < input.length; i++) {
+    if (input[i] < '9' && '0' < input[i]) totalSum += Number(input[i]);
+}
+
+console.log(totalSum);
