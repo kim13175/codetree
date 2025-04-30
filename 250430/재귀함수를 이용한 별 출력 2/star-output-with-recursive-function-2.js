547faa4d96e6237c7401152868f6283ac23b0403
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-04-30 17:24:04 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 169ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250430/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let n = Number(input[0]);
+// Please Write your code here.
+const printStar = (num) => {
+    if (num === 0) return;
+
+    for (let i = 1; i <= num; i++) {
+        process.stdout.write(`* `);
+    }
+    process.stdout.write('\n');
+
+    printStar(num - 1);
+
+    for (let i = 1; i <= num; i++) {
+        process.stdout.write(`* `);
+    }
+    process.stdout.write('\n');
+}
+
+printStar(n);
