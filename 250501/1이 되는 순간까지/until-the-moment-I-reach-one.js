55be565bbce669a809b5af0d7cabe07f0bcf0245
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-05-01 15:28:33 +0900

    Title: 1이 되는 순간까지, Time: 173ms, Memory: 15MB, Status: Passed - Codetree

--- 250501/1이 되는 순간까지/until-the-moment-I-reach-one.js
+++ 250501/1이 되는 순간까지/until-the-moment-I-reach-one.js
@@ -2,15 +2,12 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 // Please Write your code here.
-let count = 0;
 const untilOne = (num) => {
-    if (num === 1) return count;
+    if (num === 1) return 0;
     if (num % 2 === 0) {
-        count += 1;
-        return untilOne(parseInt(num / 2));
+        return untilOne(parseInt(num / 2)) + 1;
     } else {
-        count += 1;
-        return untilOne(parseInt(num / 3));
+        return untilOne(parseInt(num / 3)) + 1;
     }
 }
 
