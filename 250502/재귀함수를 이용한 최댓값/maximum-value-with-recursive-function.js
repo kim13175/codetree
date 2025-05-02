9667ba6f9e2698bca07ee4584b5d5a896bd406bd
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-05-02 12:23:22 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 5064ms, Memory: 14MB, Status: Time Limit Exceed - Codetree

--- 250502/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
+++ 250502/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
@@ -5,8 +5,8 @@ const arr = input[1].split(" ").map(Number);
 // Please Write your code here.
 const maximumNum = (num) => {
     if (num < 0) return;
-    if (maximumNum(num - 1) > arr[num]) return maximumNum(num - 1);
-    else return arr[num];
+    if (maximumNum(num - 1) > arr[num - 1]) return maximumNum(num - 1);
+    else return arr[num - 1];
 }
 
-console.log(maximumNum(n - 1))
+console.log(maximumNum(n))
