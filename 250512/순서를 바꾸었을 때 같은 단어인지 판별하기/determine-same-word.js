20d7e0a375abf599ff7f7f7c7e23b1eb834fe585
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-05-12 16:58:26 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 242ms, Memory: 20MB, Status: Passed - Codetree

--- 250512/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.js
+++ 250512/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.js
@@ -6,11 +6,14 @@ const str2 = input[1];
 
 // Please Write your code here.
 const makeSentence = (str1, str2) => {
+    if (str1.length !== str2.length) return false;
+
     str1 = str1.split('').sort();
     str2 = str2.split('').sort();
     for (let i = 0; i < str1.length; i++) {
-        if (str1[i] !== str2[i]) return false 
+        if (str1[i] !== str2[i]) return false; 
     }
+
     return true;
 }
 
