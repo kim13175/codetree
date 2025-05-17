65e340f205ccaf357461a847206f0ea413452415
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-05-17 15:05:25 +0900

    Title: 개인정보, Time: 190ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250517/개인정보/personal-info.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const students = [];
+for (let i = 0; i < 5; i++) {
+  const [name, heightStr, weightStr] = input[i].split(' ');
+  const height = Number(heightStr);
+  const weight = Number(weightStr);
+  students.push({ name, height, weight });
+}
+
+// Please Write your code here.
+class PersonInfo {
+  constructor(name, height, weight) {
+    this.name = name;
+    this.height = height;
+    this.weight = weight;
+  }
+
+  printInfo() {
+    console.log(`${this.name} ${this.height} ${this.weight}`);
+  }
+}
+
+const makeInfoClass = (arr) => {
+  infoClass = [];
+  for (let i = 0; i < arr.length; i++) {
+    infoClass.push(new PersonInfo(arr[i].name, arr[i].height, arr[i].weight));
+  }
+  return infoClass;
+}
+
+const sortName = (arr) => {
+  return [...arr].sort((prev, cur) => {
+
+    return prev.name.localeCompare(cur.name);
+  })
+}
+
+const sortHeight = (arr) => {
+  return [...arr].sort((prev, cur) => {
+    return cur.height - prev.height;
+  })
+}
+
+const printPersonList = (arr) => {
+  const sortingName = sortName(arr);
+  const sortingHeight = sortHeight(arr);
+  console.log('name');
+  for (let i = 0; i < arr.length; i++) {
+    sortingName[i].printInfo();
+  }
+
+  console.log('\n' + 'height');
+  for (let i = 0; i < arr.length; i++) {
+    sortingHeight[i].printInfo();
+  }
+}
+
+printPersonList(makeInfoClass(students));
