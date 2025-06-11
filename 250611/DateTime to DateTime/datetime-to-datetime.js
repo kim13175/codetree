4276d4adbd916b6233a592bd986de7b8089529c0
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-06-11 20:05:10 +0900

    Title: DateTime to DateTime, Time: 184ms, Memory: 15MB, Status: Passed - Codetree

--- 250611/DateTime to DateTime/datetime-to-datetime.js
+++ 250611/DateTime to DateTime/datetime-to-datetime.js
@@ -4,16 +4,19 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [A, B, C] = input[0].split(' ').map(Number);
 
 // Please write your code here.
-const prevTime = (hour, min) => {
-    if (hour < 11) return -1;
-    if (hour === 11 && min < 11) return -1;
+const prevTime = (day, hour, min) => {
+    if (day === 11) {
+        if (hour < 11) return true;
+        if (hour === 11 && min < 11) return true;
+    }
+    return false;
 }
 
 const calcMinutes = (day, hour, min) => {
-    const eleven = 11;
-    if (day === 11) prevTime(hour, min);
+    const point = 11;
+    if (prevTime(day, hour, min) === true) return -1
     
-    const startTime = (eleven * 24 * 60) + (eleven * 60) + eleven; 
+    const startTime = (point * 24 * 60) + (point * 60) + point; 
     const endTime = (day * 24 * 60) + (hour * 60) + min;
 
     return endTime - startTime;
