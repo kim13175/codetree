a40e7ba62bef64036999b643c7fb307dd1904713
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-06-11 17:04:46 +0900

    Title: Date to Date, Time: 182ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250611/Date to Date/date-to-date.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+
+// Please Write your code here.
+class Date {
+    constructor(month, date) {
+        this.month = month;
+        this.date = date;
+    }
+}
+
+const start = new Date(m1, d1);
+const end = new Date(m2, d2);
+
+
+const monthDate = (month) => {
+    if (month < 8) {
+        if (month % 2 !== 0) {
+            if (month === 2) return 28;
+            return 30;
+        } else {
+            return 31;
+        }
+    } else {
+        if (month % 2 !== 0) {
+            return 30;
+        } else {
+            return 31;
+        }
+    }
+}
+
+const calcDate = (start, end) => {
+    let countDate = 1;
+
+    while(true) {
+        if (start.month === end.month && start.date === end.date) {
+            break;
+        }
+        countDate += 1;
+        start.date += 1;
+        if (start.date === monthDate(start.month)) {
+            start.month += 1;
+            start.date = 0; 
+        }
+    }
+
+    return countDate;
+}
+
+console.log(calcDate(start, end));
