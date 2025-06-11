a81c443a4248b16a4a4ef627e4b40fda86819220
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-06-11 20:00:03 +0900

    Title: DateTime to DateTime, Time: 171ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250611/DateTime to DateTime/datetime-to-datetime.js
+++ 250611/DateTime to DateTime/datetime-to-datetime.js
@@ -7,20 +7,16 @@ const [A, B, C] = input[0].split(' ').map(Number);
 const prevTime = (hour, min) => {
     if (hour < 11) return -1;
     if (hour === 11 && min < 11) return -1;
-    return;
 }
 
-
 const calcMinutes = (day, hour, min) => {
-    const differDay = day - 11;
-    const differHour = (hour > 11) ? hour - 11 : hour - 11 + 24;
-    const differMin = (min > 11) ? min - 11 : min - 11 + 60;
-
+    const eleven = 11;
     if (day === 11) prevTime(hour, min);
-    if (hour < 11) differDay -= 1;
-    if (min < 11) differHour -= 1;
+    
+    const startTime = (eleven * 24 * 60) + (eleven * 60) + eleven; 
+    const endTime = (day * 24 * 60) + (hour * 60) + min;
 
-    return (differDay * 24 * 60) + (differHour * 60) + differMin;
+    return endTime - startTime;
 }
 
 console.log(calcMinutes(A, B, C));
