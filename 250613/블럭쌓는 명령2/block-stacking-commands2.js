50d781b55a458e4796fe94f79f86910690a7e2c8
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-06-13 12:13:28 +0900

    Title: 블럭쌓는 명령2, Time: 182ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250613/블럭쌓는 명령2/block-stacking-commands2.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = input[0].split(' ').map(Number);
+const segments = input.slice(1, k + 1).map(line => line.split(' ').map(Number));
+
+// Please write your code here.
+const amassBlock = (segments) => {
+    const block = new Array(n).fill(0).map(Number);
+    for (let i = 0; i < segments.length; i++) {
+        for (let j = segments[i][0]; j <= segments[i][1]; j++) block[j] += 1;
+    }
+    return block;
+}
+
+const totalBlock = amassBlock(segments);
+console.log(Math.max(...totalBlock));
