daf6e27ded6c66cb12f71ad2b0e621298138b70e
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-06-15 17:55:17 +0900

    Title: 흰검 칠하기, Time: 202ms, Memory: 30MB, Status: Wrong Answer - Codetree

--- 250615/흰검 칠하기/painting-white-black.js
+++ 250615/흰검 칠하기/painting-white-black.js
@@ -3,6 +3,8 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
 const commands = input.slice(1).map(line => line.split(' '));
+const offset = 100000;
+const report = new Array(2 * offset + 1).fill(0).map(Number);
 
 // Please Write your code here.
 const recordLocation = (commands) => {  
@@ -22,11 +24,42 @@ const recordLocation = (commands) => {
             end = point + move;
             point += move;
         }
-        end = point;
         location.push([start, end]);
     }
     return location;
 };
 
+const checkColor = (arr) => {
+    let colorArray = new Array(3).fill(0).map(Number);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= 5) colorArray[2] += 1;
+        else {
+            if (arr[i] % 2 === 0 && arr[i] !== 0) colorArray[0] += 1;
+            if (arr[i] % 2 !== 0) colorArray[1] += 1;
+        }
+    }
+    return colorArray;
+}
+
 const location = recordLocation(commands);
-console.log(location);
+location.forEach((loc) => {
+    let [x1, x2] = loc;
+
+    x1 += offset;
+    x2 += offset;
+
+    for (let i = x1; i < x2; i++) report[i] += 1;
+});
+
+let white = 0, black = 0, gray = 0;
+for (let i = 0; i < report.length; i++) {
+    if (report[i] >= 5) {
+        gray += 1;
+    } else if (report[i] % 2 === 0 && report[i] !== 0) {
+        white += 1;
+    } else if (report[i] % 2 !== 0) {
+        black += 1;
+    }
+}
+
+console.log(white, black, gray);
