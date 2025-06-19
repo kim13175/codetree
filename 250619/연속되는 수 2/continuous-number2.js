3e10119aa9440b02544bfde90b367bd71b897e60
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-06-19 21:59:33 +0900

    Title: 연속되는 수 2, Time: 207ms, Memory: 15MB, Status: Passed - Codetree

--- 250619/연속되는 수 2/continuous-number2.js
+++ 250619/연속되는 수 2/continuous-number2.js
@@ -8,12 +8,13 @@ const arr = input.slice(1, n + 1).map(Number);
 const checkSequence = (arr) => {
     let cnt = 1;
     let max_cnt = 0;
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] === arr[i - 1]) cnt += 1;
-        else {
-            max_cnt = Math.max(max_cnt, cnt);
+    for (let i = 1; i <= arr.length; i++) {
+        if (arr[i] === arr[i - 1]) {
+            cnt += 1;
+        } else {
             cnt = 1;
         }
+        max_cnt = Math.max(max_cnt, cnt);
     }
     return max_cnt;
 }
