e39e66671a4b00069afc16a93f510790a6305fd6
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-06-20 19:52:18 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 183ms, Memory: 15MB, Status: Passed - Codetree

--- 250620/T를 초과하는 연속 부분 수열/subsequence-above-t.js
+++ 250620/T를 초과하는 연속 부분 수열/subsequence-above-t.js
@@ -9,18 +9,13 @@ const specialNumArray = (arr, t) => {
     let cnt = 0;
     let max_cnt = 0;
     for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > t) {
-            if (arr[i - 1] < arr[i] || cnt === 0) cnt += 1;
-            else {
-                max_cnt = Math.max(cnt, max_cnt);
-                cnt = 1;
-            }
+        if (i === 0 || arr[i] > t && arr[i - 1] > t) {
+            cnt += 1;
         } else {
-            max_cnt = Math.max(cnt, max_cnt);
-            cnt = 0;
+            cnt = 1;
         }
+        max_cnt = Math.max(cnt, max_cnt);
     }
-    max_cnt = Math.max(cnt, max_cnt);
 
     return max_cnt;
 }
