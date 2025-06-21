a4b46330175a0969a9a718debbbd1752058b2582
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-06-21 18:10:18 +0900

    Title: 벌금은 누구에게, Time: 212ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250621/벌금은 누구에게/who-will-pay.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const [n, m, k] = input[0].split(" ").map(Number);
+const penalizedPersons = input.slice(1, m + 1).map(Number);
+
+const checkDemerit = (m) => {
+    let students = Array(n).fill(Number(0));
+    for (let i = 0; i < m; i++) {
+        students[penalizedPersons[i]] += 1;
+    }
+    return students;
+}
+
+const choseStudent = (arr, k) => {
+    let choosen = -1;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= k) choosen = i; 
+    }
+    return choosen;
+}
+
+const studentsArray = checkDemerit(m);
+const studentIndex = choseStudent(studentsArray, k);
+console.log(studentIndex);
