9201f541b98fb50e765258b83a6fce6c1ee62b4f
kim13175 <100892364+kim13175@users.noreply.github.com>
2025-09-10 22:40:05 +0900

    Title: 1이 3개 이상 있는 위치, Time: 78ms, Memory: 18MB, Status: Passed - Codetree

--- 250910/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 250910/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -4,21 +4,24 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
 
+def count_block(x, y):
+    cnt = 0
+    for direction in range(4):
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+        if (nx < n and ny < n and nx >= 0 and ny >= 0): 
+            if (grid[nx][ny] == 1):
+                cnt += 1            
+    return cnt
+
 def solve():
-    x, y = 0, 0;
     total_cnt = 0;
     for x in range(n):
-        cnt = 0;
         for y in range(n):
-            for direction in range(4):
-                nx = x + dx[direction]
-                ny = y + dy[direction]
-                if (nx < n and ny < n): 
-                    if (grid[nx][ny] == 1):
-                        cnt += 1
+            cnt = count_block(x, y)
             if (cnt >= 3):
                 total_cnt += 1
-    return cnt
+    return total_cnt
 
 
 print(solve())
